f9fab32e6a708f01af31f099bb6f9ef87b7e12ce
rudiliu-ma <rudi.liu@unipin.com>
2020-05-14 22:31:20 +0800

    update to Hapi

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const Hapi = require('@hapi/hapi');
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
